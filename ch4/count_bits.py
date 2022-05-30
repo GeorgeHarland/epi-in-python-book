08928ecc6f6d6cd0f8a8cac5c8ee3fa920cc667f
George Harland <georgeharland12@gmail.com>
2022-05-30 23:53:53 +0100

    first exe

--- ch4/count_bits.py
+++ ch4/count_bits.py
@@ -1,1 +1,12 @@
-# ...
+# Differently optimised to count the number of bits that are set to 1 in a nonnegative integer.
+
+# For each digit: +1 to count if bit is 1, then move to the right bit until no more bits
+# Starts with the least-significant bit
+# Time complexity is O(n), where n is the number of bits used to represent the integer
+def count_bits_start(x: int) -> int:
+    num_bits = 0
+    while (x):
+        num_bits += x & 1
+        x = x >> 1
+    return num_bits
+print(count_bits_start(47))
